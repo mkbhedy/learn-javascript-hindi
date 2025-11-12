dd66801089133bf2570194982b0a6e77abc446b9
Manish Bhedy <mkbhedy@gmail.com>
2025-11-12 06:42:12 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+// dataTypes
+"use strict"; // treat all JS Code as newer version
+
+// alert("Hello") // use in Browser not in nodejs
+
+console.log("Manish"); console.log("Kumar") // readability ke liye sahi nahi hai.  (not recommanded)
+
+/* 
+Best Documentation of learning 
+MDN by Mozilla
+Official Documentation : tc39.es
+*/
+
+// PREMETIVE DATATYPES
+
+let name = "Manish" // String dataType=> single quote and double quote
+let age = 28 // Numberic dataType
+let isLoggedIn = false // Boolean dataType=> true/false
+// bigint dataType => bada number dene ke liye
+// null dataType => iska matlab wo variable khaali hai naa ki 0 / Standalone Value 
+// undefined => value assign nahi hui hai.
+// Symbol => find unqiueness
+
+
+// OBJECTS
+
+console.log(typeof "Manish"); // type dekhne ke liye
+console.log(typeof null); // Type is Object
+console.log(typeof undefined); // Type is undefined
+
