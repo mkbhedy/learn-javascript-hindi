334a63efa2bcd7d1bcf802feb7933055e9adefda
Manish Bhedy <mkbhedy@gmail.com>
2025-11-12 15:28:47 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+//  console.log("2" > 1); // JS ne string ko number me change kr diya but comparable har baar sahi nahi hota.
+ console.log("02" < 1);
+ console.log(null > 1);
+ console.log(null == 0);
+ console.log(null >= 0);
+ 
+ // == Euality Check
+ // === Strict check + Equality Check
+ // > Greater then
+ // < less then
+ // >= greater equal
+ // <= less equal
+
+ // ye sab comparision jo console log me hai wo recommanded nahi hai
+ 
+ 
+ 
