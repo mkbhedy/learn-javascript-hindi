e3969c918f33fc4e7ebed9ada39d0a33aa4c65b9
Manish Bhedy <mkbhedy@gmail.com>
2025-11-12 06:56:11 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = 45;
+// console.log(typeof score); // 1st method
+// console.log(typeof(score)); // 2nd method
+let score01 = "mansi45";
+// console.log(typeof score01);
+
+let valueNumber = Number(score01)
+// console.log(typeof valueNumber);
+// console.log(valueNumber); // NaN => Not a Number mean usme kuch number se alag bhi value hai.
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// true/false => 1/0
+// undefined => NaN
+
+let isLoggedIn = 1;
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 => true: 0 => false
+// "" => false
+// "manish" => true
+
