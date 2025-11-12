6d8c1e0f86a6ae76dfa8c00d0f482ef063ce99d5
Manish Bhedy <mkbhedy@gmail.com>
2025-11-12 07:39:09 +0000

    operations

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,33 @@
+// OPERATIONS
+
+let value = 3;
+let negValue = -value;
+// console.log(value, negValue);
+
+// console.log(2+2); // addition
+// console.log(2-2); // subtraction
+// console.log(2*2); // Multiply
+// console.log(2**2); // Power
+// console.log(2/2); // Divided
+// console.log(2%2); // Reminder
+
+let str1 = "Manish "
+let str2 = "Jangra"
+let str3 = str1 + str2
+// console.log(str3);
+
+// Complex Situation
+
+// console.log(1 + '2');
+// console.log("2" + 1);
+// console.log("1" + 1 + 1); // 1st value string hai to puri string maani jayegi.
+// console.log(1 + 1 + "1"); // 1st value number hai to wo number maana jayega full console
+
+// console.log(+true); // not recommanded
+// console.log(+""); // not recommanded
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2 // not recommanded
+
+// prefix-postfix js mdn
+
