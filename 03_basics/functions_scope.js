0b8353ed57ef4b8df4b9bf4965a29137b5aa7842
Manish Bhedy <mkbhedy@gmail.com>
2025-11-15 12:01:10 +0000

    scope

--- 03_basics/functions_scope.js
+++ 03_basics/functions_scope.js
@@ -11,3 +11,16 @@ if(true){
 const name2 = 20
 var name3 = 30 
 }
+
+// nested function
+
+function one(){
+   const username = "manish"
+
+   function two(){
+      const website = "youtube"
+      console.log(username);
+   }
+   two()
+}
+one()
