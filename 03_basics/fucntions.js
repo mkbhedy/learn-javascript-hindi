e0453476fba9464a48809eae6aaf76070aff0679
Manish Bhedy <mkbhedy@gmail.com>
2025-11-15 06:19:58 +0000

    introduce js

---
+++ 03_basics/fucntions.js
@@ -0,0 +1,20 @@
+// functions
+ 
+/*
+ek package bnaate jisko multiple jagah par easily istemaal ksar sakte hai. usi ko functions bolte hai 
+
+function function_name(parameter1, parameter2) {
+logic and commands
+}
+
+function_name(arguments1, agruments2)
+
+*/
+
+// function addMyNumber(num1, num2) {
+//     // let result = num1 + num2
+//     // return result // result ko dene ke baad ka kuch bhi print nahi hota hai
+//     return num1 + num2
+// }
+// let myResult = addMyNumber(2,3)
+// console.log(myResult);
